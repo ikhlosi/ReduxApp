32b0a664e5c9d9db27cfd161e8ffc30d3896bc89
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2023-05-07 12:57:21 +0200

    Persistence

--- App.js
+++ App.js
@@ -2,17 +2,20 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import PostsList from "./src/features/PostsList";
-import store from "./src/store";
+import { store, persistor } from "./src/store";
 import AddPostForm from "./src/features/posts/AddPostForm";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Posts:</Text>
-        <PostsList />
-        <AddPostForm />
-      </View>
+      <PersistGate loading={null} persistor={persistor}>
+        <View style={styles.container}>
+          <Text>Posts:</Text>
+          <PostsList />
+          <AddPostForm />
+        </View>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,28 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 import postsReducer from "../features/posts/postsSclice";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   posts: postsReducer,
 });
 
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+
+// Wrap the rootReducer with the persistedReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Install the persistReducer instead of the rootReducer
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
